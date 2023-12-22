8ccb151a46e264fc3f39f87d288797cbed700f3d
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2023-12-22 17:13:11 +0100

    runtime defined HtmlElements

---
+++ react/dynamic-element-type.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+interface User{
+    name: string,
+    position: string;
+}
+const users=[
+    {name: "Uwe",  position: "Manager"},
+    {name: "Karl", position: "Superviser"},
+    {name: "Ivan", position: "Slave"}
+]
+
+//// Dynamic component, set component type without hardcoded value
+
+// using property as name of HtmlElement
+function UserDescription(props: any){
+    // name of the constant must start with upper case to be a ElementName
+    const WrapElement = props.wrapper;
+    return <>
+            <WrapElement>Name:</WrapElement> {props.user.name}
+            <hr />
+    </>
+}
+
+// using property as HtmlElement
+function ItalicText(props: any){
+    return <i>{props.children}</i>
+}
+function UserDescription2(props: any){
+    // name of the constant must start with upper case to be a ElementName
+    return <>
+            <props.wrapper>Name:</props.wrapper> {props.user.name}
+            <hr />
+    </>
+}
+
+// root element
+function App() {
+    const returnValue = [];
+    for(const eachUser of users){
+        // using wrapper-property as name of HtmlElement
+        returnValue.push( <UserDescription   user={eachUser} wrapper="b" /> );
+        // using wrapper-property as HtmlElement
+        returnValue.push( <UserDescription2  user={eachUser} wrapper={ItalicText} /> );
+    }
+  return (
+    <ul>
+        {returnValue}
+    </ul>
+  );
+}
+
+// render root
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
